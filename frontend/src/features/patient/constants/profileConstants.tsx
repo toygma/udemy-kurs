8d675159e8c3dbebd69fc4fadc1f883dfc1703fb
toygma <toygunbektasoglu@gmail.com>
2025-12-08 19:53:02 +0300

    feat: update build scripts and remove unused constants

--- frontend/src/features/patient/constants/profileConstants.tsx
+++ frontend/src/features/patient/constants/profileConstants.tsx
@@ -1,15 +1,1 @@
-import type { UserProfile } from "../types/profile.types";
-
-export const userData:UserProfile = {
-  _id: "1",
-  name: "Ahmet Yılmaz",
-  email: "ahmet.yilmaz@example.com",
-  phone: "+90 422 123 45 67",
-  gender: "Erkek",
-  address: {
-    street: "Atatürk Caddesi No:12",
-    city: "İstanbul",
-    zipCode: "34000",
-    country: "Türkiye",
-  },
-};
+//sabit veriler
